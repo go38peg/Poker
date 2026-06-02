7561e824efaf4817f9467e76648dfd6abd78a6ee
go38peg <s.luise.schill@gmail.com>
2026-06-02 13:43:36 +0000

    Co-authored-by: caiNike <caiNike@users.noreply.github.com>

--- evaluator.py
+++ evaluator.py
@@ -12,14 +12,27 @@ from cards import Card
 
 class HandCategory(IntEnum):
     # TODO: Task 1 - define the hand categories in order from lowest to highest
+    HIGH_CARD = 0
+    ONE_PAIR = 1
+    TWO_PAIR = 2
+    THREE_OF_A_KIND = 3
+    STRAIGHT = 4
+    FLUSH = 5
+    FULL_HOUSE = 6
+    FOUR_OF_A_KIND = 7
+    STRAIGHT_FLUSH = 8
     pass
 
 
 @dataclass(frozen=True, order=True)
 class HandRank:
     # TODO: Task 2 - define the HandRank dataclass with the category and tiebreakers
+    category: HandCategory
+    tiebreakers: Tuple[int, ...]
 
     # TODO: Task 2 - return a version of the category
+    def get_category(self) -> str:
+        return self.category.name.replace("_", " ").title()
     pass
 
 
@@ -27,7 +40,11 @@ class HandEvaluator:
     # TODO: Task 3 - implement the best_rank method to evaluate the best possible hand from a list of cards
     def best_rank(self, cards: list[Card]) -> HandRank:
         if len(cards) < 5:
+            raise ValueError
             pass 
+        possible_hands = combinations(cards,5)
+        all_ranks = [self._rank_five(list(combo)) for combo in possible_hands]
+        return max(all_ranks)
         pass 
 
     def _rank_five(self, cards: list[Card]) -> HandRank:
